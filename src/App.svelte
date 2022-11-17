d9722b0d6e5f5d55cb78ad2d3d94dc48faa37cf0
papa-rreyes <rreyes@papa.com>
2022-11-17 17:17:52 -0500

    transitions

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,8 @@
 <script>
   import Favorites from "@/lib/favorites/Favorites.svelte";
   import Search from "@/lib/search/Search.svelte";
-  import FakeWeather from "./lib/complications/weather/Weather.svelte";
-  import FakeSunriseSunset from "./lib/complications/weather/SunriseSunset.svelte";
+  import Weather from "./lib/complications/weather/Weather.svelte";
+  import SunriseSunset from "./lib/complications/weather/SunriseSunset.svelte";
   import Complication from "./lib/complications/Complication.svelte";
   import Loading from "./lib/complications/Loading.svelte";
 </script>
@@ -10,11 +10,8 @@
 <main class="flex flex-col items-center">
   <Favorites />
   <section class="flex flex-wrap gap-3 px-2">
-    <FakeWeather />
-    <FakeSunriseSunset />
+    <Weather />
+    <SunriseSunset />
     <Search />
   </section>
 </main>
-
-<style>
-</style>
--- src/lib/complications/Complication.svelte
+++ src/lib/complications/Complication.svelte
@@ -4,7 +4,7 @@
 
 <a
   {href}
-  class="basis-full rounded-lg bg-neutral-900 py-2 px-4 text-left font-medium text-neutral-200 active:bg-neutral-700 hover:bg-neutral-600 md:basis-auto md:text-center"
+  class="basis-full rounded-lg bg-neutral-900 py-2 px-4 text-left font-medium text-neutral-200 transition duration-200 hover:bg-neutral-600 active:bg-neutral-700 md:basis-auto md:text-center"
 >
   <aside>
     <slot>Lorem ipsum dolor sit amet</slot>
